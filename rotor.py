e7b98485bbec54e08d25a0cdcfcf17a5de46398d
Riane404 <rianegreat@gmail.com>
2025-04-29 00:06:57 +0530

    created rotor

---
+++ rotor.py
@@ -0,0 +1,20 @@
+class Rotor:
+    def __init__(self, wiring: str, notch: str, position='A'):
+        self.wiring = wiring
+        self.notch = ord(notch.upper()) - ord('A')
+        self.position = ord(position.upper()) - ord('A')
+
+    def encode_forward(self, c: str) -> str:
+        index = (ord(c.upper()) - ord('A') + self.position) % 26
+        encoded_c = self.wiring[index]
+        return chr((ord(encoded_c) - ord('A') - self.position + 26) % 26 + ord('A'))
+
+    def encode_backward(self, c: str) -> str:
+        index = (ord(c.upper()) - ord('A') + self.position) % 26
+        letter = chr((index + ord('A')) % 256)
+        pos_in_wiring = self.wiring.index(letter)
+        return chr((pos_in_wiring - self.position + 26) % 26 + ord('A'))
+
+    def rotate(self) -> bool:
+        self.position = (self.position + 1) % 26
+        return self.position == self.notch
